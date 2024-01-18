12f010ca3ea981d4990d7b933373ae868cb05738
Kavindhya Nirmani <124445752+KavindhyaNirmani@users.noreply.github.com>
2024-01-18 11:25:37 +0530

    add image

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,25 +1,32 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import img from "../assest/Fimg.png"; // Assuming the image is in the same directory as this component
+import "./Home.css"; // Create a new CSS file for styling
 
 function Home(props) {
   return (
-    <div>
-      <div>
-      <h1>
+    <div className="home-container">
+      <div className="left-side">
+        <img src={img} alt="Image Description" />
+      </div>
+
+      <div className="right-side">
+        <h1>
           <Link to="/signup">Register</Link>
         </h1>
         <br />
         <h1>
           <Link to="/login">Login</Link>
         </h1>
-        
+         <br/>
+        <h1>{props.name ? `Welcome - ${props.name}` : "Login please"}</h1>
       </div>
 
       <br />
       <br />
       <br />
 
-      <h2>{props.name ? `Welcome - ${props.name}` : "Login please"}</h2>
+    
     </div>
   );
 }
